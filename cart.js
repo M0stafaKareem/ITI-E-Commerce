71fd60548409b1b9785e8a50920bdb8f4419d4af
YousefHSS <yousefhussen139@gmail.com>
2024-07-14 02:10:48 +0300

    Cart Functionality Done

--- cart.js
+++ cart.js
@@ -1,4 +1,6 @@
 let CartData ={};
+const MinCountValue = 1;
+const MaxCountValue = 100;
 async function AddToCart(itemID) {
 
         if (CartData[itemID] != null) {
@@ -26,34 +28,55 @@ async function AddToCart(itemID) {
     
 }
 
-function  removeFromCart(itemID) {
+function  RemoveFromCart(itemID) {
     if (CartData[itemID][0] > 1) {
         CartData[itemID][0] = CartData[itemID][0] - 1;
-        updateBadgeAndCart(CartData[itemID][1]);
+        updateBadgeAndCart();
     }
     else
     {
         delete CartData[itemID];
-        updateBadgeAndCart(CartData[itemID][1]);
+        var CurrentSessionCart = JSON.parse(sessionStorage.getItem('cart'));
+        delete CurrentSessionCart[itemID];
+        sessionStorage.setItem('cart', JSON.stringify(CurrentSessionCart));
+        updateBadgeAndCart();
     }
 }
 
 
-function updateBadgeAndCart(ItemJson){
-    const price = document.querySelector('#Price');
+
+function CountAllItems(){
+    let Count = 0;
+    for (const [key, value] of Object.entries(CartData)) {
+        Count += value[0];
+    }
+    return Count;
+}
+function calculateTotalPrice() {
+    // Convert CartData's values into an array of [quantity, {price: x}] arrays
+    const itemsArray = Object.values(CartData);
+    // Use reduce on the array to calculate the total price
+    const totalPrice = itemsArray.reduce((sum, item) => sum + (item[1].price * item[0]), 0);
+    return parseFloat(totalPrice).toFixed(2) + " E£"; //return totalPrice;
+}
+function updateBadgeAndCart(){
+    const price = document.querySelector('#CartTotalSum');
     const badge = document.querySelector('#badge');
+    if(price != null){
+        price.innerText = calculateTotalPrice();
+    }
     if(badge != null){
-            if(badge.classList.contains('badge')){
-            badge.innerText = parseInt(badge.innerText) + 1;
-            //add price trimmed the price
-            price.innerText = parseInt(price.innerText) + parseInt(ItemJson.price);
-            price.innerText = parseInt(price.innerText).toFixed(2);
+        
+        if(badge.classList.contains('badge')){
+            badge.innerText = CountAllItems();
+            //multiply price by count and sum it
+           
+            // price.innerText = parseInt(price.innerText).toFixed(2);
         }
         else{
             badge.classList.add('badge');
             badge.innerText = (0) + 1;
 
-            price.innerText = ItemJson.price;
         }
     }
     PopulateCartPage();
@@ -62,99 +85,158 @@ function updateBadgeAndCart(ItemJson){
 
 function AddItemToCart(itemID) {
 
-    const cart = JSON.parse(sessionStorage.getItem('cart'));
-    if(cart == null){
-        sessionStorage.setItem('cart', JSON.stringify([itemID]));
+    var SessionCart = JSON.parse(sessionStorage.getItem('cart'));
+    if(SessionCart == null){
+
+        sessionStorage.setItem('cart', JSON.stringify([{itemID : 1}]));
         AddToCart(itemID);
+
     }
     else{
-        cart.push(itemID);
-        sessionStorage.setItem('cart', JSON.stringify(cart));
+        //get the right item
+        
+        if(SessionCart[itemID] != null){
+            SessionCart[itemID]++;
+        }
+        else{
+            SessionCart[itemID]=1;
+        }
+
+        sessionStorage.setItem('cart', JSON.stringify(SessionCart));
         AddToCart(itemID);
     }
 }
-
-function UpdateSessionStorage(){
-    var Array=[];
-    Object.entries(CartData).forEach(([key, CarItem]) => {
-       for (let index = 0; index < CarItem[0]; index++) {
+async function SetToCart(itemID , Count){
+    
+    if (Count<MinCountValue) {
         
-        Array.push(key);
-       } 
-    })
-    sessionStorage.setItem('cart', JSON.stringify(Array));
+        Count = MinCountValue;
+        CartData[itemID] = Count;
+        RemoveFromCart(itemID);
+        console.log(CartData);
+        PopulateCartPage();
+        UpdateSessionStorage();
+        
+        
+    }else{
+        if (Count>MaxCountValue)
+            {
+                Count = MaxCountValue;
+            }
+            if (CartData[itemID] != null) {
+                            
+                CartData[itemID][0] = Count;
+                updateBadgeAndCart(CartData[itemID][1]);
+            }
+            else
+            {
+                await fetch('https://dummyjson.com/products/' + itemID)
+                .then(res => {
+                    //store the json result in a variable
+                    return res.json();
+                })
+                .then(ItemJson => {
+                        
+                    var InnerArray = [];
+                    InnerArray.push(Count);
+                    InnerArray.push(ItemJson);
+                    CartData[itemID] = InnerArray;
+                    updateBadgeAndCart(ItemJson);
+                });
+            }
+            PopulateCartPage();
+            UpdateSessionStorage();
+    }
+    
+}
 
+function UpdateSessionStorage() {
+    let simplifiedCartData = {};
+    Object.entries(CartData).forEach(([key, CartItem]) => {
+        simplifiedCartData[key] = CartItem[0]; // Assuming CartItem[0] is the count
+    });
+    sessionStorage.setItem('cart', JSON.stringify(simplifiedCartData));
 }
 
 //on document load
 document.addEventListener('DOMContentLoaded', async function() {
     await checkSession();
-    console.log(CartData);
     var CartGrid = document.querySelector('.CartGrid');
     if(CartGrid != null && CartData !== null){
         PopulateCartPage();
        
     }
     else{
-        console.log('Cart is empty');
+
     }
 });
 
 function PopulateCartPage(){
     const CartGrid = document.querySelector('.CartGrid');
     if (CartGrid!=null && CartData != null){ 
-            console.log(CartData);
         CartGrid.innerHTML = '';
         //because CartData is a 2D array and not always filled continuously we need to loop through it using for each
         Object.entries(CartData).forEach(([key, CarItem]) => {
-            console.log(CartData);
-            console.log(CarItem);
+
         
             CartGrid.innerHTML += `
-            <div class="CartItems">
-                <img src="${CarItem[1].thumbnail}">
-                <div class="ItemInfo">
-                    <div class="ItemName">${CarItem[1].title}</div>
-                    <div class="ItemPrice"><div style="font-weight: lighter;"><span style="font-weight: bold;">Price:</span> <span id="Price">${CarItem[1].price}</span> <span  >EGP</span> </div></div>
-                    <div class="ItemRemove"><button style="cursor: pointer;float: left;" onclick='DeleteAllFromCart(${CarItem[1].id})' class="btn btn-remove"><i class="fa-solid fa-trash"></i>     Remove</button></div>
-                </div> 
-                <div class="CountControl">
-                    <i style="cursor: pointer;" onclick="AddItemToCart(${key})" class="fa-solid fa-plus greenIcon"></i>
-                    <span class="transparentIcon">${CarItem[0]}</span>
-                    <i style="cursor: pointer;" onclick="removeFromCart(${key})" class="fa-solid fa-minus greenIcon"></i>
+            <tr>
+            <td>
+                <div class="CartItemImage">
+                    <img width="100" src="${CarItem[1].thumbnail}"> 
                 </div>
-            </div>
-            <hr>
+            </td>
+            <td><div class="ItemName">
+                ${CarItem[1].title}</div></td>
+            <td><div class="CartTableCenter">${parseFloat(CarItem[1].price).toFixed(2)} E£</div></td>
+            <td><div class="CartTableCenter"><div class="CountControlContainer"><i onclick="RemoveFromCart(${CarItem[1].id})" class="fa-solid fa-minus borderedIcon CountControl"></i><input type="number" onchange="SetToCart(${CarItem[1].id},this.value)" class="MidIcon borderedIcon " min="1" max="100" value="${CarItem[0]}"><i onclick="AddToCart(${CarItem[1].id})" class="fa-solid fa-plus borderedIcon CountControl"></i></div></td>
+            <td style="position: relative;"><div class="CartTableCenter">${parseFloat(CarItem[1].price * CarItem[0]).toFixed(2)} E£</div> <div onclick="SetToCart(${CarItem[1].id},0)" class="Remove_Link" style="bottom: .5rem;position: absolute;padding: 5px;/*! font-size: 2r; */">Remove</div></td>
+        </tr>
             `;
         });
     }
    
        
     
-    console.log(CartData);
+
 }
 
 
   async function checkSession(){
     //if there is data set in the session storage for the cart fetch them and display them
     if(sessionStorage.getItem('cart') != null){
-        var cart = JSON.parse(sessionStorage.getItem('cart'));
-        console.log(cart.length);
+        var SessionCart = JSON.parse(sessionStorage.getItem('cart'));
+
         //for each item in the cart
-        for(var i = 0; i < cart.length; i++){
-            
-            await AddToCart(cart[i]);
-        }
+        await Object.entries(SessionCart).forEach(([key, CarItem]) => {   
+             SetToCart(key,CarItem);
+        });
        
     }
     else{
-        console.log('Cart Session Storage is empty');
+
     }
   }
 
   function clearSession(){
     sessionStorage.removeItem('cart');
   }
+
+
+function ToggleSideCart(){
+    const SideCart = document.querySelector('#HideSideCart');
+    SideCart.classList.toggle('HideSideCart');
+
+}
+
+document.querySelector('#HideSideCart').addEventListener('click',
+function(event){
+    ToggleSideCart();
+});
+document.querySelector('.SideCart').addEventListener('click',
+function(event){
+    event.stopPropagation();
+});
 
 
   
